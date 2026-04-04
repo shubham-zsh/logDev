b70590f86c25c5c828aa8dd91f3b44aaa2b88bab
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-04 17:03:19 +0530

    login route complete

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,17 +1,25 @@
 import prisma from '@/lib/prisma';
+import bcrypt from "bcryptjs";
 
 export async function POST(request) {
 
-    const {username, password} = await request.json();
+    const { username, password } = await request.json();
 
     const user = await prisma.user.findFirst({
-    where: { username: username }
+        where: { username: username }
     })
-    
+
     if (!user) {
         return (
-            Response.json({msg: "user does not exits..."})
+            Response.json({ msg: "user does not exits..." }, { status: 404 })
         )
     }
-    return Response.json({message: "signed in done"})
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return (
+            Response.json({ msg: "password is wrong" }, { status: 401 })
+        )
+    }
+    return Response.json({ message: "logged in successfully" }, { status: 200 })
 }
