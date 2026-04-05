00d35af8c7cf17e54960ccd6aedda5e7ac9de28f
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-05 20:20:38 +0530

    jwt implemented

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,5 +1,8 @@
 import prisma from '@/lib/prisma';
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'
+import { NextResponse } from 'next/server'
 
 export async function POST(request) {
 
@@ -21,5 +24,21 @@ export async function POST(request) {
             Response.json({ msg: "password is wrong" }, { status: 401 })
         )
     }
-    return Response.json({ message: "logged in successfully" }, { status: 200 })
+    const response = NextResponse.json(
+        { message: "logged in successfully" },
+        { status: 200 }
+    )
+
+    const token = jwt.sign(
+        { userId: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    )
+
+    response.cookies.set('token', token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7
+    })
+
+    return response;
 }
