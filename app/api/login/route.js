1afe9eb68008a16489cbf89e61011b92f8d461b2
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-07 12:34:51 +0530

    improved login and register api rotues

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,44 +1,49 @@
 import prisma from '@/lib/prisma';
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
-import 'dotenv/config'
 import { NextResponse } from 'next/server'
 
 export async function POST(request) {
 
-    const { username, password } = await request.json();
+    try {
+        const { username, password } = await request.json();
 
-    const user = await prisma.user.findFirst({
-        where: { username: username }
-    })
+        const user = await prisma.user.findFirst({
+            where: { username: username }
+        })
 
-    if (!user) {
-        return (
-            Response.json({ msg: "user does not exits..." }, { status: 404 })
+        if (!user) {
+            return (
+                Response.json({ msg: "user does not exits..." }, { status: 404 })
+            )
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return (
+                Response.json({ msg: "password is wrong" }, { status: 401 })
+            )
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
         )
-    }
 
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) {
-        return (
-            Response.json({ msg: "password is wrong" }, { status: 401 })
+        const response = NextResponse.json(
+            { message: "logged in successfully" },
+            { status: 200 }
         )
+
+        response.cookies.set('token', token, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7
+        })
+
+        return response;
+
+    } catch (err) {
+        return Response.json({ msg: "Something went wrong..." }, { status: 500 })
     }
-    const response = NextResponse.json(
-        { message: "logged in successfully" },
-        { status: 200 }
-    )
-
-    const token = jwt.sign(
-        { userId: user.id, username: user.username },
-        process.env.JWT_SECRET,
-        { expiresIn: '7d' }
-    )
-
-    response.cookies.set('token', token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 7
-    })
-
-    return response;
 }
--- app/api/register/route.js
+++ app/api/register/route.js
@@ -3,27 +3,32 @@ import prisma from '@/lib/prisma';
 
 export async function POST(request) {
 
-    const { username, password } = await request.json()
-    const hashedPassword = await bcrypt.hash(password, 10)
+    try {
+        const { username, password } = await request.json()
 
-    const userExists = await prisma.user.findFirst({
-        where: { username: username }
-    });
+        const userExists = await prisma.user.findFirst({
+            where: { username: username }
+        });
 
-    if (userExists) {
-        return Response.json({ msg: "user already exists" }, { status: 409 })
-    }
-
-    const user = await prisma.user.create({
-        data: {
-            username: username,
-            password: hashedPassword
+        if (userExists) {
+            return Response.json({ msg: "user already exists" }, { status: 409 })
         }
-    });
 
-    if (!user) {
-        return Response.json({ msg: "something went wrong" }, { status: 500 });
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: hashedPassword
+            }
+        });
+
+        if (!user) {
+            return Response.json({ msg: "something went wrong" }, { status: 500 });
+        }
+
+        return Response.json({ msg: "register successfull" }, { status: 201 })
+    } catch (err) {
+        return Response.json({ msg: "Something went wrong" }, { status: 500 })
     }
-
-    return Response.json({ msg: "register successfull" }, { status: 201 })
 }
