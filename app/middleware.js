e74f3a691779d3f770510d706f67dcdf87954e9c
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-07 18:23:51 +0530

    completed middleware

--- app/middleware.js
+++ app/middleware.js
@@ -1,9 +1,25 @@
-import { jetVerify } from 'jose';
+import { jwtVerify } from 'jose';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
+    const url = new URL('/login', request.url);
+    const tokenCookie = request.cookies.get('token');
 
-    const token = request.cookies.get('token');
+    if (!tokenCookie) {
+        return NextResponse.redirect(url);
+    }
 
-    
+    const token = tokenCookie.value;
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    try {
+        await jwtVerify(token, secret);
+        return NextResponse.next();
+    } catch (err) {
+        return NextResponse.redirect(url);
+    }
 }
+
+export const config = {
+    matcher: ['/dashboard'],
+};
