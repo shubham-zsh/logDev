6a1ea7383d0ed24fd52bfb31a428e94ace78187e
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-10 11:49:33 +0530

    added navigation after login

--- app/login/page.js
+++ app/login/page.js
@@ -1,44 +1,53 @@
 'use client';
+
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const router = useRouter();
 
     async function handleLogin() {
-
         try {
-            const res = await fetch('/api/login',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ username, password })
-                })
+            const res = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
 
             if (!res.ok) {
-                throw new Error('Request failed ' + res.status)
+                throw new Error('Request failed ' + res.status);
             }
-            const data = await res.json();
 
-            console.log(data)
-
+            router.push('/dashboard');
         } catch (err) {
             console.log(err);
         }
-
-
     }
 
-    return (<div>
-        <h1>login page</h1>
+    return (
+        <div>
+            <h1>Login Page</h1>
 
-        <input type='text' placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
 
-        <button onClick={handleLogin}>login </button>
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
-    </div>)
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
 }
