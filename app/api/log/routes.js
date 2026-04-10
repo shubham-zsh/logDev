d5ba18130cecd0b3a4dfd5977345ccce1d74b1f2
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-10 11:32:53 +0530

    added get route for logs

--- app/api/log/routes.js
+++ app/api/log/routes.js
@@ -1,19 +1,32 @@
 import prisma from "@/lib/prisma";
 
-export async function POST(req) {
-
-    const { content } = await req.json();
-    const userId = req.header.get('userId');
-
+export async function POST(request) {
     try {
+        const { content } = await request.json();
+        const userId = parseInt(request.headers.get("userId"));
+
         const log = await prisma.log.create({
-            data: { content, userId }
-        })
+            data: { content, userId },
+        });
+
         console.log(log);
 
-        return Response.json({ msg: "log created successfully.." }, { status: 201 })
-
+        return Response.json({ msg: "Log created successfully." }, { status: 201 });
     } catch (err) {
-        return Response.json({ msg: "Something went wrong..." }, { status: 500 })
+        return Response.json({ msg: "Something went wrong..." }, { status: 500 });
+    }
+}
+
+export async function GET(request) {
+    try {
+        const userId = parseInt(request.headers.get("userId"));
+
+        const logs = await prisma.log.findMany({
+            where: { userId },
+        });
+
+        return Response.json({ logs }, { status: 200 });
+    } catch (err) {
+        return Response.json({ msg: "Something went wrong..." }, { status: 500 });
     }
 }
