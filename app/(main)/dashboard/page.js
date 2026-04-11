20cd7ab33d650741e1df6f4a30e1a82459e47721
Shubham Borse <shubhamborse0911@gmail.com>
2026-04-11 16:28:29 +0530

    added fetch logs in dash

--- app/(main)/dashboard/page.js
+++ app/(main)/dashboard/page.js
@@ -1,10 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Dashboard() {
+
     const [contents, setContent] = useState('');
     const [logs, setLogs] = useState([]);
+
+    useEffect(() => {
+        fetchLogs()
+    }, []);
+
+    async function fetchLogs() {
+
+        try {
+            const res = await fetch('/api/logs');
+
+            if (!res) {
+                return Response.json({ msg: "failed to fetch" })
+            }
+            console.log(res);
+
+            const data = res.json();
+            console.log(data);
+            setLogs(data.logs)
+        } catch (err) {
+            console.error("error", err);
+        }
+    }
 
     return (
         <div>
@@ -13,6 +36,8 @@ export default function Dashboard() {
             <div>
                 <input type="text" placeholder="log" value={contents} onChange={(e) => setContent(e.target.value)} />
             </div>
+
+
 
             <div>
                 {logs.map((log) => {
